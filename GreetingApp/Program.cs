b1a0ee264d1bc4daa82693fe84b62bb719295bb9
flores.313371 <flores.313371@LAB-A-PC38>
2024-02-26 12:15:54 +0800

    Add project files.

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter the total number of your enrolled courses: ");
+            int totalcourses;
+            totalcourses = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the price of your favorite book");
+            double price;
+            price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total number of enrolled courses: " + totalcourses);
+            Console.WriteLine("Price of my favorite book: " + price);
+
+        }
+    }
+}
